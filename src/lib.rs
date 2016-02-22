977230a76b3c122fe76ec9a532f3a082cae2d0f5
Antoine Gourlay <antoine@gourlay.fr>
2016-02-22 20:30:34 +0100

    add a config window from a GLADE file.
    Also move everything into Player.

--- src/lib.rs
+++ src/lib.rs
@@ -8,39 +8,61 @@ use std::sync::Arc;
 
 use gtk::traits::*;
 use gtk::Window;
+use gtk::widgets::Builder;
 use gtk::signal::Inhibit;
 use vlc::MediaPlayerVideoEx;
 
 mod ffi;
 pub mod gtk_window;
 
+// macro for getting stuff from a builder
+macro_rules! builder_get {
+    ( $b:expr, $name:expr, $typ:ty ) => {
+        unsafe { $b.get_object::<$typ>($name).expect("$name") }
+    }
+}
+
 pub struct Player {
     vlc_instance: vlc::Instance,
     media_player: vlc::MediaPlayer,
-    window: gtk::Window,
+    builder: gtk::widgets::Builder,
+    play_window: gtk::Window,
+    configure_window: gtk::Window,
+    control_window: gtk::Window,
 }
 
 impl Player {
     pub fn new() -> Arc<Player> {
-        let window = gtk::Window::new(gtk::WindowType::Toplevel).unwrap();
+        let glade_src = include_str!("config_window.glade");
+        let builder = Builder::new_from_string(glade_src).expect("can't find builder");
+
+        let play_window = gtk::Window::new(gtk::WindowType::Toplevel).expect("play_window");
+        let configure_window = builder_get!(&builder, "configure_window", Window);
+        let control_window = gtk::Window::new(gtk::WindowType::Toplevel).expect("control_window");
+
         let instance = vlc::Instance::new().unwrap();
         let mdp = vlc::MediaPlayer::new(&instance).unwrap();
 
         let p = Arc::new(Player {
             vlc_instance: instance,
             media_player: mdp,
-            window: window,
+            builder: builder,
+            play_window: play_window,
+            configure_window: configure_window,
+            control_window: control_window,
         });
 
-        p.window.connect_show({
+        p.style_windows();
+
+        p.play_window.connect_show({
             let pp = p.clone();
             move |_| {
-                let id = gtk_window::gdk_x11_window_get_xid(&pp.window.get_window().unwrap());
+                let id = gtk_window::gdk_x11_window_get_xid(&pp.play_window.get_window().unwrap());
                 pp.media_player.set_xwindow(id);
             }
         });
 
-        p.window.connect_delete_event({
+        p.control_window.connect_delete_event({
             let pp = p.clone();
             move |_, _| {
                 pp.media_player.stop();
@@ -50,7 +72,35 @@ impl Player {
             }
         });
 
-        p.style_window();
+        p.play_window.connect_delete_event({
+            let pp = p.clone();
+            move |_, _| {
+                pp.media_player.stop();
+                Inhibit(false)
+            }
+        });
+
+        p.configure_window.connect_delete_event(|_, _| {
+            gtk::main_quit();
+            Inhibit(false)
+        });
+
+        builder_get!(&p.builder, "button_start", gtk::Button).connect_clicked({
+            let pp = p.clone();
+            move |_| {
+                pp.configure_window.hide();
+                pp.play_window.show_all();
+                pp.control_window.show_all();
+                pp.media_player.play().unwrap();
+            }
+        });
+
+        builder_get!(&p.builder, "button_quit", gtk::Button).connect_clicked({
+            let pp = p.clone();
+            move |_| {
+                gtk::main_quit();
+            }
+        });
 
         p
     }
@@ -80,12 +130,19 @@ impl Player {
         self.media_player.set_fullscreen(fullscren);
     }
 
+    pub fn configure(&self) {
+        self.configure_window.show_all();
+    }
+
     // internal stuff
 
-    fn style_window(&self) {
-        self.window.set_title("Presenter");
-        self.window.set_default_size(850, 670);
-        self.window.set_window_position(gtk::WindowPosition::Center);
-        self.window.show_all();
+    fn style_windows(&self) {
+        self.play_window.set_title("Presenter Output");
+        self.play_window.set_default_size(850, 670);
+        self.play_window.set_window_position(gtk::WindowPosition::Center);
+
+        self.control_window.set_title("Controller");
+        self.control_window.set_default_size(100, 32);
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,69 +14,66 @@ fn main() {
         return;
     }
 
-    let args: Vec<String> = std::env::args().collect();
-    let path = match args.get(1) {
-        Some(s) => s,
-        None => {
-            println!("Usage: movpres <path_to_media_file>");
-            return;
-        }
-    };
-
     let pl = Player::new();
 
-    pl.set_media(path);
+    let args: Vec<String> = std::env::args().collect();
+    match args.get(1) {
+        Some(s) => pl.set_media(&s),
+        None => (),
+    };
 
-    let control_window = gtk::Window::new(gtk::WindowType::Toplevel).unwrap();
-    control_window.set_title("Controller");
-    control_window.set_default_size(100, 32);
+    pl.configure();
 
-    let buttons = gtk::Box::new(gtk::Orientation::Horizontal, 0).unwrap();
-    let play_image = gtk::Image::new_from_icon_name("media-playback-start",
-                                                    gtk::IconSize::Button as i32)
-                         .unwrap();
-    let play_button = gtk::Button::new().unwrap();
-    play_button.add(&play_image);
-
-    let stop_image = gtk::Image::new_from_icon_name("media-playback-stop",
-                                                    gtk::IconSize::Button as i32)
-                         .unwrap();
-    let stop_button = gtk::Button::new().unwrap();
-    stop_button.add(&stop_image);
-
-    let fullscreen_button = gtk::ToggleButton::new_with_label("Fullscreen").unwrap();
-
-    buttons.pack_start(&play_button, false, false, 0);
-    buttons.pack_start(&stop_button, false, false, 0);
-    buttons.pack_start(&fullscreen_button, false, false, 0);
-
-    play_button.connect_clicked({
-        let ppl = pl.clone();
-        move |_| {
-            ppl.toggle_play();
-        }
-    });
-    stop_button.connect_clicked({
-        let ppl = pl.clone();
-        move |_| {
-            ppl.stop();
-        }
-    });
-    fullscreen_button.connect_clicked({
-        let ppl = pl.clone();
-        let fb = fullscreen_button.clone();
-        move |_| {
-            let fs = fb.get_active();
-            ppl.set_fullscreen(fs);
-        }
-    });
-    buttons.show_all();
-
-    control_window.add(&buttons);
-    control_window.show_all();
-
-    pl.play();
-
+    //    let control_window = gtk::Window::new(gtk::WindowType::Toplevel).unwrap();
+    // control_window.set_title("Controller");
+    // control_window.set_default_size(100, 32);
+    //
+    // let buttons = gtk::Box::new(gtk::Orientation::Horizontal, 0).unwrap();
+    // let play_image = gtk::Image::new_from_icon_name("media-playback-start",
+    // gtk::IconSize::Button as i32)
+    // .unwrap();
+    // let play_button = gtk::Button::new().unwrap();
+    // play_button.add(&play_image);
+    //
+    // let stop_image = gtk::Image::new_from_icon_name("media-playback-stop",
+    // gtk::IconSize::Button as i32)
+    // .unwrap();
+    // let stop_button = gtk::Button::new().unwrap();
+    // stop_button.add(&stop_image);
+    //
+    // let fullscreen_button = gtk::ToggleButton::new_with_label("Fullscreen").unwrap();
+    //
+    // buttons.pack_start(&play_button, false, false, 0);
+    // buttons.pack_start(&stop_button, false, false, 0);
+    // buttons.pack_start(&fullscreen_button, false, false, 0);
+    //
+    // play_button.connect_clicked({
+    // let ppl = pl.clone();
+    // move |_| {
+    // ppl.toggle_play();
+    // }
+    // });
+    // stop_button.connect_clicked({
+    // let ppl = pl.clone();
+    // move |_| {
+    // ppl.stop();
+    // }
+    // });
+    // fullscreen_button.connect_clicked({
+    // let ppl = pl.clone();
+    // let fb = fullscreen_button.clone();
+    // move |_| {
+    // let fs = fb.get_active();
+    // ppl.set_fullscreen(fs);
+    // }
+    // });
+    // buttons.show_all();
+    //
+    // control_window.add(&buttons);
+    // control_window.show_all();
+    //
+    // pl.play();
+    //
     gtk::main();
 }
 
